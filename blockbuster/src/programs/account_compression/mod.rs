87e55daccb55b6949802ea975852f5feb387a81a
Ghulam Mustafa Raza Qadri <ghulammustafasoomar@gmail.com>
2023-11-25 10:49:08 +0000

    Added , Debug for Instruction enum in account_compression_parser

--- blockbuster/src/programs/account_compression/mod.rs
+++ blockbuster/src/programs/account_compression/mod.rs
@@ -22,7 +22,7 @@ use anchor_lang::Discriminator;
 use spl_account_compression::instruction::*;
 
 fn get_instruction_type(full_bytes: &[u8]) -> Instruction {
-    let (disc_slice, full_bytes) = full_bytes.split_at(8);
+    let (disc_slice, args_bytes) = full_bytes.split_at(8);
     let disc: [u8; 8] = {
         let mut disc = [0; 8];
         disc.copy_from_slice(&disc_slice);
@@ -31,14 +31,14 @@ fn get_instruction_type(full_bytes: &[u8]) -> Instruction {
 
     match disc {
         InitEmptyMerkleTree::DISCRIMINATOR => {
-            let init_empty_merkle_tree = InitEmptyMerkleTree::try_from_slice(&full_bytes).unwrap();
+            let init_empty_merkle_tree = InitEmptyMerkleTree::try_from_slice(&args_bytes).unwrap();
             Instruction::InitTree {
                 max_depth: init_empty_merkle_tree.max_depth,
                 max_buffer_size: init_empty_merkle_tree.max_buffer_size,
             }
         }
         ReplaceLeaf::DISCRIMINATOR => {
-            let replace_leaf = ReplaceLeaf::try_from_slice(&full_bytes).unwrap();
+            let replace_leaf = ReplaceLeaf::try_from_slice(&args_bytes).unwrap();
             Instruction::ReplaceLeaf {
                 root: replace_leaf.root,
                 previous_leaf: replace_leaf.previous_leaf,
@@ -47,13 +47,13 @@ fn get_instruction_type(full_bytes: &[u8]) -> Instruction {
             }
         }
         TransferAuthority::DISCRIMINATOR => {
-            let transfer_authority = TransferAuthority::try_from_slice(&full_bytes).unwrap();
+            let transfer_authority = TransferAuthority::try_from_slice(&args_bytes).unwrap();
             Instruction::TransferAuthority {
                 new_authority: transfer_authority.new_authority,
             }
         }
         VerifyLeaf::DISCRIMINATOR => {
-            let verify_leaf = VerifyLeaf::try_from_slice(&full_bytes).unwrap();
+            let verify_leaf = VerifyLeaf::try_from_slice(&args_bytes).unwrap();
             Instruction::VerifyLeaf {
                 root: verify_leaf.root,
                 leaf: verify_leaf.leaf,
@@ -61,11 +61,11 @@ fn get_instruction_type(full_bytes: &[u8]) -> Instruction {
             }
         }
         Append::DISCRIMINATOR => {
-            let append = Append::try_from_slice(&full_bytes).unwrap();
+            let append = Append::try_from_slice(&args_bytes).unwrap();
             Instruction::Append { leaf: append.leaf }
         }
         InsertOrAppend::DISCRIMINATOR => {
-            let insert_or_append = InsertOrAppend::try_from_slice(&full_bytes).unwrap();
+            let insert_or_append = InsertOrAppend::try_from_slice(&args_bytes).unwrap();
             Instruction::InsertOrAppend {
                 root: insert_or_append.root,
                 leaf: insert_or_append.leaf,
@@ -77,7 +77,7 @@ fn get_instruction_type(full_bytes: &[u8]) -> Instruction {
     }
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Debug)]
 pub enum Instruction {
     Unknown,
     InitTree {
@@ -113,7 +113,6 @@ pub struct AccountCompressionInstruction {
     pub instruction: Instruction,
     pub tree_update: Option<ChangeLogEventV1>,
     pub leaf_update: Option<ApplicationDataEventV1>,
-    // pub payload: Option<Payload>,
 }
 
 impl AccountCompressionInstruction {
@@ -122,7 +121,6 @@ impl AccountCompressionInstruction {
             instruction: ix,
             tree_update: None,
             leaf_update: None,
-            // payload: None,
         }
     }
 }
@@ -218,80 +216,6 @@ impl ProgramParser for AccountCompressionParser {
             }
         }
 
-        // if outer_ix_data.len() >= 8 {
-        //     let ix_data = &outer_ix_data[8..];
-        //     if !ix_data.is_empty() {
-        //         match b_inst.instruction {
-        //             InstructionName::MintV1 => {
-        //                 let args: MetadataArgs = MetadataArgs::try_from_slice(ix_data)?;
-        //                 b_inst.payload = Some(Payload::MintV1 { args });
-        //             }
-        //             InstructionName::MintToCollectionV1 => {
-        //                 let mut args: MetadataArgs = MetadataArgs::try_from_slice(ix_data)?;
-        //                 if let Some(ref mut col) = args.collection {
-        //                     col.verified = true;
-        //                 }
-        //                 b_inst.payload = Some(Payload::MintV1 { args });
-        //             }
-        //             InstructionName::DecompressV1 => {
-        //                 let args: MetadataArgs = MetadataArgs::try_from_slice(ix_data)?;
-        //                 b_inst.payload = Some(Payload::Decompress { args });
-        //             }
-        //             InstructionName::CancelRedeem => {
-        //                 let slice: [u8; 32] = ix_data
-        //                     .try_into()
-        //                     .map_err(|_e| BlockbusterError::InstructionParsingError)?;
-        //                 b_inst.payload = Some(Payload::CancelRedeem { root: slice });
-        //             }
-        //             InstructionName::VerifyCreator => {
-        //                 b_inst.payload = Some(build_creator_verification_payload(keys, true)?);
-        //             }
-        //             InstructionName::UnverifyCreator => {
-        //                 b_inst.payload = Some(build_creator_verification_payload(keys, false)?);
-        //             }
-        //             InstructionName::VerifyCollection | InstructionName::SetAndVerifyCollection => {
-        //                 b_inst.payload = Some(build_collection_verification_payload(keys, true)?);
-        //             }
-        //             InstructionName::UnverifyCollection => {
-        //                 b_inst.payload = Some(build_collection_verification_payload(keys, false)?);
-        //             }
-        //             InstructionName::Unknown => {}
-        //             _ => {}
-        //         };
-        //     }
-        // }
-
         Ok(Box::new(b_inst))
     }
 }
-
-// // See AccountCompression documentation for offsets and positions:
-// // https://github.com/metaplex-foundation/mpl-AccountCompression/blob/main/programs/AccountCompression/README.md#-verify_creator-and-unverify_creator
-// fn build_creator_verification_payload(
-//     keys: &[plerkle_serialization::Pubkey],
-//     verify: bool,
-// ) -> Result<Payload, BlockbusterError> {
-//     let creator = keys
-//         .get(5)
-//         .ok_or(BlockbusterError::InstructionParsingError)?
-//         .0;
-//     Ok(Payload::CreatorVerification {
-//         creator: Pubkey::new_from_array(creator),
-//         verify,
-//     })
-// }
-
-// // See AccountCompression for offsets and positions:
-// // https://github.com/metaplex-foundation/mpl-AccountCompression/blob/main/programs/AccountCompression/README.md#-verify_collection-unverify_collection-and-set_and_verify_collection
-// // NOTE: Unverfication does not include collection. This needs to be fixed in the README.
-// fn build_collection_verification_payload(
-//     keys: &[plerkle_serialization::Pubkey],
-//     verify: bool,
-// ) -> Result<Payload, BlockbusterError> {
-//     let collection_raw = keys
-//         .get(8)
-//         .ok_or(BlockbusterError::InstructionParsingError)?
-//         .0;
-//     let collection: Pubkey = Pubkey::try_from_slice(&collection_raw)?;
-//     Ok(Payload::CollectionVerification { collection, verify })
-// }
